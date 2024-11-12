11caa72377bedd98491729c043d6c5526f55e7dd
Rafi <rafi.haykal2007@smkn4-tsm.sch.id>
2024-11-12 13:41:14 +0700

    login&register

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\Admin\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [LoginRegisterController::class, 'register'])->name('register');
+    Route::get('/store', [LoginRegisterController::class, 'store'])->name('store');
+    Route::get('/login', [LoginRegisterController::class, 'login'])->name('login');
+    Route::get('/authenticate', [LoginRegisterController::class, 'authenticate'])->name('authenticate');
+    
+});
+
+Route::middleware('auth','admin')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin/dashboard');
+    Route::get('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
+});
